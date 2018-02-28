6f5dd151bc38495fea7c514c0fcdebb9ccc3eb3e
lincchenl <lincchenl@gmail.com>
2018-03-01 03:28:37 +0800

    developing version

--- dnn.py
+++ dnn.py
@@ -2,13 +2,6 @@
 import numpy as np
 import pickle
 
-def normflat(data):
-	remap=data.flatten()
-	mean=np.mean(remap)
-	deviation=np.std(remap)
-	remap=(remap-mean)/deviation
-	return remap
-
 class BN_layer:
 	length = int(0)
 	batch_cnt = int(0)
@@ -57,81 +50,18 @@ class BN_layer:
 		self.x_mean=np.mean(self.x,axis=0)
 		self.x_std=np.var(self.x,axis=0)
 		self.x_norm=(self.x-self.x_mean)/np.sqrt(self.x_std+self.epsilon)
-		self.result=self.gamma*self.x_norm+self.beta
+		self.result=self.x_norm*np.tile(self.gamma,x.shape[1]).reshape(x.shape)
+		self.result+=np.tile(self.beta,x.shape[1]).reshape(x.shape)
 
 	def backward(self,dout): #dout is the total m output diffs
 		x_mu=self.x-self.x_mean
 		std_inv=1./np.sqrt(self.x_std+self.epsilon)
-		self.dx_norm = np.multiply(dout,self.gamma)
+		self.dx_norm = np.multiply(dout,np.tile(self.gamma,dout.shape[1]).reshape(dout.shape))
 		self.dx_std = np.sum(self.dx_norm * x_mu, axis=0) * -.5 * std_inv ** 3
 		self.dx_mean = np.sum(self.dx_norm * -std_inv, axis=0) + self.dx_std * np.mean(-2. * x_mu, axis=0)
 		self.dx = (self.dx_norm * std_inv) + (self.dx_std * 2 * x_mu / self.batch_cnt) + (self.dx_mean / self.batch_cnt)
-		self.dgamma = np.sum(dout * self.x_norm, axis=0)
-		self.dbeta = np.sum(dout, axis=0)
-
-class BN_layer:
-	length = int(0)
-	batch_cnt = int(0)
-	epsilon = 1e-8
-	gamma = None
-	beta = None
-	#foward
-	x = None
-	x_mean = None
-	x_std = None
-	x_norm = None
-	result = None
-	fwd_flag = False
-	#backward
-	dx_norm = None
-	dx_std = None
-	dx_mean = None
-	dbeta = None
-	dgamma = None
-	dx = None
-	bwd_flag = False
-
-	def __del__(self):
-		self.gamma = None
-		self.beta = None
-		# foward
-		self.x = None
-		self.x_mean = None
-		self.x_std = None
-		self.x_norm = None
-		self.result = None
-		# backward
-		self.dx_norm = None
-		self.dx_std = None
-		self.dx_mean = None
-		self.dbeta = None
-		self.dgamma = None
-		self.dx = None
-
-	def __init__(self, length,cnt):
-		self.length=length
-		self.batch_cnt=cnt
-		self.gamma=np.ones(length,dtype=np.float)
-		self.beta=np.zeros(length,dtype=np.float)
-
-	def forward(self,x):
-		self.x=x
-		self.x_mean=np.mean(self.x,axis=0)
-		self.x_std=np.var(self.x,axis=0)
-		self.x_norm=(self.x-self.x_mean)/np.sqrt(self.x_std+self.epsilon)
-		self.result=self.gamma*self.x_norm+self.beta
-		self.fwd_flag = True
-
-	def backward(self,dout): #dout is the total m output diffs
-		x_mu=self.x-self.x_mean
-		std_inv=1./np.sqrt(self.x_std+self.epsilon)
-		self.dx_norm = np.multiply(dout,self.gamma)
-		self.dx_std = np.sum(self.dx_norm * x_mu, axis=0) * -.5 * std_inv ** 3
-		self.dx_mean = np.sum(self.dx_norm * -std_inv, axis=0) + self.dx_std * np.mean(-2. * x_mu, axis=0)
-		self.dx = (self.dx_norm * std_inv) + (self.dx_std * 2 * x_mu / self.batch_cnt) + (self.dx_mean / self.batch_cnt)
-		self.dgamma = np.sum(dout * self.x_norm, axis=0)
-		self.dbeta = np.sum(dout, axis=0)
-		self.bwd_flag = True
+		self.dgamma = np.sum(dout * self.x_norm, axis=1)
+		self.dbeta = np.sum(dout, axis=1)
 
 class DO_layer:
 	length = 0
@@ -141,12 +71,56 @@ class DO_layer:
 		self.dp_array = None
 	def __init__(self, rate,cnt):
 		self.dp_rate=rate
-		self.dp_array=np.ones(cnt,dtype=np.float)
+		self.length=cnt
 	def forward(self,x):
 		self.dp_array=np.random.binomial(1, self.dp_rate, size=self.length)
 		return np.multiply(self.dp_array,x)
 	def backward(self,dout):
 		return np.multiply(self.dp_array,dout)
+
+class AC_func:
+	fNo = 0
+	x = None
+	def __init__(self,kind):
+		self.fNo=kind
+
+	def __selu(self, x):
+		alpha = 1.6732632423543772848170429916717
+		scale = 1.0507009873554804934193349852946
+		return scale * np.where(x > 0.0, x, alpha * (np.exp(x) - 1.))
+
+	def __selup(self):
+		alpha = 1.6732632423543772848170429916717
+		scale = 1.0507009873554804934193349852946
+		return scale * np.where(self.x > 0.0, 1., alpha * np.exp(self.x))
+	def __relu(self, x):
+		return np.where(x < 0., 0., x)
+
+	def __relup(self):
+		return np.where(self.x < 0., 0., 1.)
+	def __softmax(self, x):
+		e_x=np.exp(x)
+		return e_x/e_x.sum()
+	def __softmaxp(self):
+		return np.ones(self.x.shape, dtype=np.float)
+
+	def forward(self,x):
+		switch = {
+			1: self.__selu,
+			2: self.__relu,
+			3: self.__softmax
+		}
+		self.x=x
+		return switch[self.fNo](x)
+
+	def backward(self,dout):
+		switch = {
+			1: self.__selup,
+			2: self.__relup,
+			3: self.__softmaxp
+		}
+		return switch[self.fNo]()*dout
+
 
 class nn_layer:
 	layer = int(0)
@@ -154,95 +128,87 @@ class nn_layer:
 	inited = False
 	bNorm = None
 	dOut = None
+	aFunc = None
 
 	def __init__(self, count, p=None, c=None):
 		self.data = np.zeros(count, dtype=np.float)
-		self.partial = np.zeros(count, dtype=np.float)
 		self.count = count
 		self.parent = p
 		self.child = c
 		self.inited = True
+		self.diff = np.empty([self.count], dtype=np.float)
 
 	def __del__(self):
 		self.child = None
 		self.parent = None
 		self.bNorm = None
 		self.dOut = None
+		self.aFunc = None
 
 	def append(self, p=None):
 		if isinstance(p, nn_layer):
 			self.child = p.child
 			self.parent = p
 			p.child = self
-			if p.layer == -1:
-				p.layer = p.parent.layer + 1
 			self.layer = p.layer + 1
-			if not self.child:
-				self.layer = -1
 			# 分配空间存储参数
-			self.para = np.zeros([self.count, p.count + 1], dtype=np.float)
-			self.diff = np.zeros([self.count], dtype=np.float)
-			self.dpara = np.zeros(self.para.shape, dtype=np.float)
+			self.para = np.empty([self.count, p.count + 1], dtype=np.float)
+			self.dpara = np.empty([self.count, p.count + 1], dtype=np.float)
 			# adam
 			self.mt = 0.
 			self.vt = 0.
-			# dropout
-			self.dprate = 1.  # 默认禁用dropout
 
-	def __selu(self, x):
-		alpha = 1.6732632423543772848170429916717
-		scale = 1.0507009873554804934193349852946
-		return scale * np.where(x > 0.0, x, alpha * (np.exp(x) - 1.))
-
-	def __selup(self, x):
-		alpha = 1.6732632423543772848170429916717
-		scale = 1.0507009873554804934193349852946
-		return scale * np.where(x > 0.0, 1., alpha * np.exp(x))
-
-	def __relu(self, x):
-		return np.where(x < 0., 0., x)
-
-	def __relup(self, x):
-		return np.where(x < 0., 0., 1.)
-
-	def __softmax(self, x):
-		e_x=np.exp(x)
-		return e_x/e_x.sum()
-
-	def __softmaxp(self, x):
-		return np.ones(x.shape, dtype=np.float)
-
-	def backward_1step(self, result):
+	# 每一层的处理顺序是 x->BN->全连接->激活函数->dropout->y
+	# forward的时候就是按照这样传递data
+	# backward的时候刚好顺序是相反的，传递的是diff
+	def backward_1step(self, result,BN_No):
 		my_cnt = self.data.size
-		parent_cnt = self.parent.data.size
-		if not self.child:
+		cd=self.child
+		if not cd:
+			# 输出层
 			self.diff = result - self.data
 		else:
-			child_cnt = self.child.data.size
+			# 通过child的diff来求本层的diff
+			child_cnt=cd.diff.size
+			dout=cd.diff
+			# dropout
+			if cd.dOut :
+				dout=cd.dOut.backward(dout)
+			# 激活函数
+			if cd.aFunc:
+				dout=cd.aFunc.backward(dout)
 			self.diff.fill(0.)
 			for j in range(child_cnt):
-				self.diff[:] += self.child.diff[j] * self.child.para[j, :my_cnt]
-		var_x = self.parent.data
-		self.dpara[:, parent_cnt] = self.diff[:] * self.partial[:]
-		for j in range(parent_cnt):
-			self.dpara[:, j] = self.dpara[:, parent_cnt] * var_x[j]
+				self.diff[:] += dout[j] * cd.para[j, :my_cnt]
+			# Batch Normalization在循环外完成
+			# 计算dpara
+			if cd.bNorm:
+				var_x = cd.bNorm.result[:,BN_No]
+			else:
+				var_x = self.data
+			cd.dpara[:, my_cnt] = dout
+			for j in range(my_cnt):
+				cd.dpara[:, j] = cd.dpara[:, my_cnt] * var_x[j]
 
-	def forward_1step(self, train):
+
+	def forward_1step(self, train,BN_No):
 		parent_cnt = self.parent.data.size
-		lresult = np.dot(self.para[:, :parent_cnt], self.parent.data) + self.para[:, parent_cnt]
-		if not self.child:
-			result = self.__softmax(lresult)
-			self.partial = self.__softmaxp(lresult)
-		else:
-			result = self.__selu(lresult)
-			self.partial = self.__selup(lresult)
-		# dropout
-		#if train:
-		#	dropout = np.random.binomial(1, self.dprate, size=self.data.shape)
-		#	np.multiply(result,dropout,out=result)
-		#	np.multiply(self.partial,dropout,out=self.partial)
-		#else:
-		#	np.multiply(result, self.dprate, out=result)
+		# Batch Normalization在循环外完成
+		if self.bNorm:
+			input=self.bNorm.result[:,BN_No]
+		else :
+			input=self.parent.data
+		# 全连接层
+		result = np.dot(self.para[:, :parent_cnt], input) + self.para[:, parent_cnt]
+		# 激活函数
+		if self.aFunc:
+			result = self.aFunc.forward(result)
+		# dropout层
+		if self.dOut:
+			if train:
+				result=self.dOut.forward(result)
+			else:
+				result=self.dOut.dp_rate
 		self.set_data(result)
 
 	def set_para(self, data):
@@ -300,15 +266,6 @@ class nn:
 			p.set_para(np.random.uniform(0.01 * 0.01, 0.02 * 0.02, pshape))
 			p = p.child
 
-	def train_input(self, map_to_train, result_no):
-		remap=normflat(map_to_train)
-		self.top.set_data(remap)
-		result = np.zeros(self.tail.data.shape, dtype=np.float)
-		result[result_no] = 1.0
-		self.forward(True)
-		self.backward(result)
-		return sum(self.tail.diff ** 2) / 2.
-
 	def para_commit(self, cat, ratio):
 		# apply the ADAM algorithm to speedup the converge of the SGD
 		p = self.tail
@@ -316,6 +273,7 @@ class nn:
 		beta2 = 0.999
 		epsilon = 1e-8
 		while p.parent:
+			#adam
 			gt = p.dpara
 			alpha = ratio / float(cat)
 			p.mt = beta1 * p.mt + (1. - beta1) * gt
@@ -324,30 +282,6 @@ class nn:
 			vt_mean = p.vt / (1. - beta2)
 			gt.fill(0.)
 			p.set_para(p.para + mt_mean / np.sqrt(vt_mean + epsilon) * alpha)
-			p = p.parent
-
-	def test(self, map_tt):
-		remap=normflat(map_tt)
-		self.top.set_data(remap)
-		self.forward(False)
-
-	def clear(self):
-		p = self.top.child
-		while p:
-			p.mt.fill(0.)
-			p.vt.fill(0.)
-			p = p.child
-
-	def forward(self, train):
-		p = self.top.child
-		while p:
-			p.forward_1step(train)
-			p = p.child
-
-	def backward(self, result):
-		p = self.tail
-		while p.parent:
-			p.backward_1step(result)
 			p = p.parent
 
 	def copy_para_dnn(self, src):
--- main.py
+++ main.py
@@ -13,15 +13,28 @@ def testndraw(data, nn_object):
 	plt.imshow(data, cmap='gray')
 	plt.show()
 
+# 对原始图片进行标准化处理
+def normflat(data):
+	remap=data.flatten()
+	mean=np.mean(remap)
+	deviation=np.std(remap)
+	remap=(remap-mean)/deviation
+	return remap
 
 # initiate a nn
 def creatnn():
 	nn_object = dnn.nn(28 * 28)
 	nn_object.addlayer(100)
+	nn_object.locate(1).bNorm=bn1
+	#nn_object.locate(1).dOut=dnn.DO_layer(0.8,100)
+	nn_object.locate(1).aFunc=dnn.AC_func(2) #relu
 	nn_object.addlayer(100)
+	nn_object.locate(2).bNorm=bn2
+	#nn_object.locate(2).dOut=dnn.DO_layer(0.8,100)
+	nn_object.locate(2).aFunc=dnn.AC_func(2) #relu
 	nn_object.addlayer(10)
-	nn_object.locate(1).dprate=0.8
-	nn_object.locate(2).dprate=0.8
+	nn_object.locate(3).bNorm=bn3
+	nn_object.locate(3).aFunc=dnn.AC_func(3) #softmax
 	return nn_object
 
 
@@ -46,23 +59,25 @@ if __name__ == "__main__":
 	fl_mgc, fl_cnt = struct.unpack(">II", fl[0:8])
 	fl_idx = np.asarray(fl[8:])
 	# 读取图片文件
+	print ("读取文件中......")
 	fp = np.fromfile(path_images, dtype=np.uint8)
 	fp_mac, fp_cnt, fp_row, fp_col = struct.unpack(">IIII", fp[0:16])
 	fp_img = np.asarray(fp[16:])
 	fp_img.resize([fp_cnt, fp_row, fp_col])
-	# category
-	no_idx = np.zeros((fl_cnt, 10), dtype=np.int)
-	index = np.zeros(10, dtype=np.int)
-	for i in range(fl_cnt):
-		j = int(fl_idx[i])
-		no_idx[index[j], j] = i
-		index[j] += 1
-	group = np.min(index)
+	# 对图片进行预处理
+	print("图片预处理......")
+	img=np.empty((28*28,fp_cnt),dtype=np.float)
+	for i in range(fp_cnt):
+		img[:,i]=normflat(fp_img[i,:,:])
 
-	nn1 = creatnn()
-	nn1.train_prepare()
+
 	cat = 100
 	group = 10000
+	bn1 = dnn.BN_layer(28 * 28, cat)
+	bn2 = dnn.BN_layer(100, cat)
+	bn3 = dnn.BN_layer(100, cat)
+	nn1 = creatnn()
+	nn1.train_prepare()
 	first = True
 	error = np.zeros(group, dtype=float)
 	rs = np.zeros(cat, dtype=float)
@@ -86,19 +101,66 @@ if __name__ == "__main__":
 			while res > 1e-5 and cnt < 2:
 				rs.fill(0.)
 				k = 0
-				# cleanup
-				# the cleanup work has been done int he para_commit seciton
+				nns = []
+				result = np.zeros([10,cat],dtype=np.float)
 				for j in sgd:
-					# 此处循环可以用多个线程来实现
-
-					tmp_nn = creatnn()
-					tmp_nn.copy_para_dnn(nn1)
-
-					rs[k] = tmp_nn.train_input(fp_img[j, :, :], fl_idx[j])
+					# 新建cat个nn网络
+					nns.append(creatnn())
+					nns[k].copy_para_dnn(nn1)
+					# 准备好cat个输入
+					nns[k].top.set_data(img[:,j])
+					result[fl_idx[j],k] = 1.0
 					k = k + 1
-					# 累加结果
-					for j in range(1, nn1.layers):
-						nn1.locate(j).dpara += tmp_nn.locate(j).dpara / cat
+				#开始训练
+				#forward
+				for j in range(1,nn1.layers):
+					p=nn1.locate(j)
+					if p.bNorm:
+						#在推进之前计算本层的BN
+						x=np.empty([p.parent.data.size,cat],dtype=np.float)
+						for k in range(cat):
+							x[:,k]=nns[k].locate(j).parent.data.copy()
+						p.bNorm.forward(x)
+					for k in range(cat):
+						nns[k].locate(j).forward_1step(True,k)
+				#backward
+				for j in range(nn1.layers-1,-1,-1):
+					p = nn1.locate(j)
+					for k in range(cat):
+						nns[k].locate(j).backward_1step(result[:, k], k)
+					if p.bNorm:
+						# 准备dout
+						my_cnt = p.data.size
+						parent_cnt = p.parent.data.size
+						dout = np.empty([parent_cnt, cat], dtype=np.float)
+						for k in range(cat):
+							child_dout = nns[k].locate(j).diff
+							# dropout
+							if nns[k].locate(j).dOut:
+								child_dout = nns[k].locate(j).dOut.backward(child_dout)
+							# active func
+							if nns[k].locate(j).aFunc:
+								child_dout = nns[k].locate(j).aFunc.backward(child_dout)
+							# 全连接
+							dout[:,k].fill(0.)
+							for l in range(my_cnt):
+								dout[:, k] += dout[l,k] * nns[k].locate(j).para[l, :parent_cnt]
+						p.bNorm.backward(dout)
+				# 误差统计
+				for k in range(cat):
+					rs[k]=np.sum(nns[k].tail.diff**2)/2.
+				# 累加结果
+				for j in range(1,nn1.layers):
+					nn1.locate(j).dpara.fill(0.)
+					for k in range(1,nn1.layers):
+						nn1.locate(k).dpara += nns[j].locate(k).dpara / cat
+				# gamma和beta的学习
+				bn1.beta += bn1.dbeta * 0.1
+				bn1.gamma += bn1.dgamma * 0.1
+				bn2.beta += bn2.dbeta * 0.1
+				bn2.gamma += bn2.dgamma * 0.1
+				bn3.beta += bn3.dbeta * 0.1
+				bn3.gamma += bn3.dgamma * 0.1
 				nn1.para_commit(cat, 0.1)
 				res = sum(rs) / cat
 				cnt += 1
@@ -116,6 +178,10 @@ if __name__ == "__main__":
 	fp1_mac, fp1_cnt, fp1_row, fp1_col = struct.unpack(">IIII", fp1[0:16])
 	fp1_img = np.asarray(fp1[16:])
 	fp1_img.resize([fp1_cnt, fp1_row, fp1_col])
+	# 测试图片预处理
+	img1=np.empty((28*28,fp_cnt),dtype=np.float)
+	for i in range(fp1_cnt):
+		img1[:,i]=normflat(fp1_img[i,:,:])
 
 	print("总测试样本的个数是：", fl1_cnt, "。")
 	print("迭代100次后的准确率是：", test_accu("d:\\paras\\parad100.pkl", fl1_cnt, fp1_img, fl1_idx) * 100, "%  ")
