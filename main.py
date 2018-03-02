610f831738d85c37b3f7e13bc35571d05bfefe4c
lincchenl <lincchenl@gmail.com>
2018-03-02 21:58:04 +0800

    新程序仍然有些问题，奇怪???加了bn还会有梯度爆炸，再检查看看

--- main.py
+++ main.py
@@ -1,50 +1,49 @@
 import struct
 import numpy as np
-import dnn
+import mnn
 import matplotlib.pyplot as plt
 
 path_lables = ".\\source\\train-labels.idx1-ubyte"
 path_images = ".\\source\\train-images.idx3-ubyte"
-
-# 读取图片文件
-def testndraw(data, nn_object):
-	nn_object.test(data)
-	plt.imshow(data, cmap='gray')
-	plt.show()
+path_lables1 = ".\\source\\t10k-labels.idx1-ubyte"
+path_images1 = ".\\source\\t10k-images.idx3-ubyte"
 
 # 对原始图片进行标准化处理
 def normflat(data):
-	remap=data.ravel()
-	mean=np.mean(remap)
-	deviation=np.std(remap)
-	remap=(remap-mean)/deviation
+	remap=data.flatten()
+	#mean=np.mean(remap)
+	#deviation=np.std(remap)
+	#remap=(remap-mean)/np.sqrt(deviation)
+	remap=remap/255
 	return remap
 
 # initiate a nn
 def creatnn():
-	nn_object = dnn.nn(28 * 28)
-	nn_object.addlayer(100)
-	#nn_object.locate(1).bNorm=bn1
-	nn_object.locate(1).dOut=dnn.DO_layer(0.8,100)
-	nn_object.locate(1).aFunc=dnn.AC_func(1) #relu
-	nn_object.addlayer(100)
-	#nn_object.locate(2).bNorm=bn2
-	nn_object.locate(2).dOut=dnn.DO_layer(0.8,100)
-	nn_object.locate(2).aFunc=dnn.AC_func(1) #relu
-	nn_object.addlayer(10)
-	#nn_object.locate(3).bNorm=bn3
-	nn_object.locate(3).aFunc=dnn.AC_func(3) #softmax
-	return nn_object
-
+	nn=mnn.mnn()
+	fc1=mnn.full_connect(28*28,100)
+	fc2=mnn.full_connect(100,100)
+	fc3=mnn.full_connect(100,10)
+	fc1.opti=mnn.adam(0.1)
+	fc2.opti=mnn.adam(0.1)
+	fc3.opti=mnn.adam(0.1)
+	nn.addlayer(fc1)
+	nn.addlayer(mnn.batch_normalization())
+	nn.addlayer(mnn.active_function(1))
+	nn.addlayer(fc2)
+	nn.addlayer(mnn.batch_normalization())
+	nn.addlayer(mnn.active_function(1))
+	nn.addlayer(fc3)
+	nn.addlayer(mnn.active_function(3))
+	return nn
 
 def test_accu(filename, count, datas, results):
 	right = 0
 	wrong = 0
-	nn1=creatnn()
-	nn1=nn1.load_params(filename)
+	nn1=mnn.mnn.load(filename)
 	for index in range(count):
-		nn1.test(datas[index,:])
-		if nn1.tail.data[results[index]] > 0.5 :
+		nn1.input=datas[index:index,:]
+		nn1.forward(False)
+		if nn1.output[results[index]] > 0.5 :
 			right += 1
 		else:
 			wrong += 1
@@ -53,6 +52,7 @@ def test_accu(filename, count, datas, results):
 
 # where the main program starts
 if __name__ == "__main__":
+	# 训练数据
 	# 读取标签文件
 	fl = np.fromfile(path_lables, dtype=np.uint8)
 	fl_mgc, fl_cnt = struct.unpack(">II", fl[0:8])
@@ -69,96 +69,7 @@ if __name__ == "__main__":
 	for i in range(fp_cnt):
 		img[i,:]=normflat(fp_img[i,:,:])
 
-
-	cat = 100
-	group = 10000
-	bn1 = dnn.BN_layer(28 * 28, cat)
-	bn2 = dnn.BN_layer(100, cat)
-	bn3 = dnn.BN_layer(100, cat)
-	nn1 = creatnn()
-	nn1.train_prepare()
-	first = True
-	error = np.zeros(group, dtype=float)
-	rs = np.zeros(cat, dtype=float)
-	if first:
-		for i in range(group):
-			res = 10.0
-			cnt = 1
-			# nn1.clear()
-			if i == 100 - 1:
-				nn1.save_params("d:\\paras\\parad100.pkl")
-			if i == 1000 - 1:
-				nn1.save_params("d:\\paras\\parad1000.pkl")
-			if i == 2000 - 1:
-				nn1.save_params("d:\\paras\\parad2000.pkl")
-			if i == 5000 - 1:
-				nn1.save_params("d:\\paras\\parad5000.pkl")
-			if i == 10000 - 1:
-				nn1.save_params("d:\\paras\\parad10000.pkl")
-
-			sgd = np.random.randint(0, fl_cnt, size=cat)
-			while res > 1e-5 and cnt < 2:
-				rs.fill(0.)
-				k = 0
-				nns = []
-				result = np.zeros([cat,10],dtype=np.float)
-				for j in sgd:
-					# 新建cat个nn网络
-					nns.append(creatnn())
-					nns[k].copy_para_dnn(nn1)
-					# 准备好cat个输入
-					nns[k].top.set_data(img[j,:])
-					result[k,fl_idx[j]] = 1
-					k = k + 1
-				#开始训练
-				#forward
-				for j in range(1,nn1.layers):
-					p=nn1.locate(j)
-					if p.bNorm:
-						#在推进之前计算本层的BN
-						x=np.empty([cat,p.parent.data.size],dtype=np.float)
-						for k in range(cat):
-							x[k,:]=nns[k].locate(j).parent.data.copy()
-						p.bNorm.forward(x)
-					for k in range(cat):
-						nns[k].locate(j).forward_1step(True,k)
-				#计算误差
-				for k in range(cat):
-					diff=nns[k].tail.data-result[k,:]
-					rs[k]=np.sum(diff**2)/2.
-				#backward
-				for j in range(nn1.layers-1,0,-1):
-					p = nn1.locate(j)
-					for k in range(cat):
-						nns[k].locate(j).backward_1step(result[k,:], k)
-					if p.bNorm:
-						# 准备dout
-						dout = np.empty([cat,nn1.locate(j).diff.size], dtype=np.float)
-						for k in range(cat):
-							dout[k,:]=nns[k].locate(j).diff
-						p.bNorm.backward(dout)
-				# 累加结果
-				for j in range(1,nn1.layers):
-					nn1.locate(j).dpara.fill(0.)
-					for k in range(1,nn1.layers):
-						nn1.locate(k).dpara += nns[j].locate(k).dpara / cat
-				# gamma和beta的学习
-				for j in range(1,nn1.layers):
-					bn=nn1.locate(j).bNorm
-					if bn :
-						bn.beta += bn.dbeta
-						bn.gamma += bn.dgamma
-				# 更新全连接层的参数
-				nn1.para_commit(cat, 0.1)
-				res = sum(rs) / cat
-				cnt += 1
-				print(i, cnt - 1, res)
-			error[i] = res
-		np.save("d:\\paras\\error", error)
-	# test trained network
-	path_lables1 = ".\\source\\t10k-labels.idx1-ubyte"
-	path_images1 = ".\\source\\t10k-images.idx3-ubyte"
-	# 读取标签文件
+	# 验证数据
 	fl1 = np.fromfile(path_lables1, dtype=np.uint8)
 	fl1_mgc, fl1_cnt = struct.unpack(">II", fl1[0:8])
 	fl1_idx = np.asarray(fl1[8:])
@@ -170,6 +81,49 @@ if __name__ == "__main__":
 	img1=np.empty((fp1_cnt,28*28),dtype=np.float)
 	for i in range(fp1_cnt):
 		img1[i,:]=normflat(fp1_img[i,:,:])
+
+	cat = 100
+	group = 10000
+	nn1 = creatnn()
+	first = True
+	error = np.zeros(group, dtype=float)
+	rs = np.zeros(cat, dtype=float)
+	if first:
+		for i in range(group):
+			res = 10.0
+			cnt = 1
+			# nn1.clear()
+			if i == 100 - 1:
+				nn1.save("d:\\paras\\parad100.pkl")
+			if i == 1000 - 1:
+				nn1.save("d:\\paras\\parad1000.pkl")
+			if i == 2000 - 1:
+				nn1.save("d:\\paras\\parad2000.pkl")
+			if i == 5000 - 1:
+				nn1.save("d:\\paras\\parad5000.pkl")
+			if i == 10000 - 1:
+				nn1.save("d:\\paras\\parad10000.pkl")
+
+			sgd = np.random.randint(0, fl_cnt, size=cat)
+			while res > 1e-5 and cnt < 2:
+				k = 0
+				result = np.zeros([cat,10],dtype=np.float)
+				for j in sgd:
+					result[k,fl_idx[j]]=1
+					k+=1
+				#开始训练
+				nn1.input=img[sgd,:]
+				nn1.forward(True,result)
+				#计算误差
+				rs[:]=np.sum(nn1.diff**2./2.,axis=1)
+				#backward
+				nn1.backward()
+				res = np.average(rs)
+				cnt += 1
+				print(i, cnt - 1, res)
+			error[i] = res
+		np.save("d:\\paras\\error", error)
+
 
 	print("总测试样本的个数是：", fl1_cnt, "。")
 	print("迭代100次后的准确率是：", test_accu("d:\\paras\\parad100.pkl", fl1_cnt, img1, fl1_idx) * 100, "%  ")
--- venv/mnn.py
+++ venv/mnn.py
@@ -1,6 +1,7 @@
-#module dnn.py
+#module mnn.py
 import numpy as np
 from abc import ABCMeta, abstractmethod
+import pickle
 
 class mnn:
 	input = None # dimension [N,D]
@@ -9,9 +10,7 @@ class mnn:
 	head = None
 	tail = None
 	diff = None
-	def __init__(self,icnt,ocnt,batch_cnt):
-		self.input=np.zeros([batch_cnt,icnt],dtype=np.float)
-		self.output = np.zeros([batch_cnt, ocnt], dtype=np.float)
+
 	def addlayer(self,object):
 		self.layers=self.layers+1
 		if not self.head:
@@ -22,8 +21,8 @@ class mnn:
 			object.parent=self.tail
 		self.tail=object
 		object.child=None
-	def forward(self,train,result=None):
-		p=self.top
+	def forward(self,train=True,result=None):
+		p=self.head
 		while p :
 			if p.parent:
 				p.input=p.parent.output
@@ -34,16 +33,29 @@ class mnn:
 		self.output=self.tail.output
 		if train:
 			self.diff=result-self.output
-	def backward(self):
+	def backward(self,train=True):
 		p=self.tail
 		while p :
 			if p.child:
 				p.dout=p.child.dx
 			else:
 				p.dout=self.diff
-			p.backward()
-			p.update() #更新每一层参数，比如全连接层的系数或者BN层的beta，gamma
+			p.backward(train)
+			p.update(train) #更新每一层参数，比如全连接层的系数或者BN层的beta，gamma
 			p=p.parent
+	def save(self, filename):
+		#在保存之前先把网络的规格变成batch=1,瘦身
+		self.input=self.input[0:0,:]
+		self.forward(False)
+		self.backward(False)
+		f = open(filename, 'wb')
+		pickle.dump(self, f)
+		f.close()
+	def load(self, filename):
+		f = open(filename, 'rb')
+		obj = pickle.load(f)
+		f.close()
+		return obj
 
 # 每一层网络的抽象类，它包括输入输出，以及dx和dout四个成员数据 以及forward和backward和update三个成员函数待实现
 class layer:
@@ -64,13 +76,13 @@ class layer:
 		self.parent=None
 
 	@abstractmethod
-	def forward(self,train):
+	def forward(self,train=True):
 		pass
 	@abstractmethod
-	def backward(self):
+	def backward(self,train=True):
 		pass
 	@abstractmethod
-	def update(self):
+	def update(self,train=True):
 		pass
 # 激活函数类
 class active_function(layer):
@@ -94,7 +106,7 @@ class active_function(layer):
 		e_x=np.exp(x)
 		return e_x/e_x.sum()
 	def __softmaxp(self,x):
-		return np.ones(self.input.shape, dtype=np.float)
+		return 1.
 
 	def forward(self,train=True):
 		switch = {
@@ -102,17 +114,27 @@ class active_function(layer):
 			2: self.__relu,
 			3: self.__softmax
 		}
-		self.output=switch[self.__fNo](self.input)
+		cnt=self.input.shape[0]
+		self.output=np.zeros(self.input.shape,dtype=np.float)
+		for i in range(cnt):
+			self.output[i,:]=switch[self.__fNo](self.input[i,:])
 
-	def backward(self):
-		switch = {
-			1: self.__selup,
-			2: self.__relup,
-			3: self.__softmaxp
-		}
-		self.dx=switch[self.__fNo](self.input)*self.dout
+	def backward(self,train=True):
+		if train:
+			switch = {
+				1: self.__selup,
+				2: self.__relup,
+				3: self.__softmaxp
+			}
+			cnt = self.dout.shape[0]
+			self.dx = np.zeros(self.dout.shape, dtype = np.float)
+			for i in range(cnt):
+				self.dx[i,:] = switch[self.__fNo](self.input[i,:]) * self.dout[i,:]
+		else:
+			self.dx=None
+			self.dout=None
 
-	def update(self):
+	def update(self,train=True):
 		pass
 # Dropout类
 class dropout(layer):
@@ -123,7 +145,7 @@ class dropout(layer):
 		self.dp_array = None
 	def __init__(self, rate):
 		self.dp_rate=rate
-	def forward(self,train):
+	def forward(self,train=True):
 		if train:
 			self.dp_array=np.empty(self.input.shape,dtype=np.float)
 			for i in range(self.input.shape[0]):
@@ -131,9 +153,13 @@ class dropout(layer):
 			self.output=self.dp_array*self.input
 		else:
 			self.output=self.input*self.dp_rate
-	def backward(self):
-		self.dx=self.dout*self.dp_array
-	def update(self):
+	def backward(self,train=True):
+		if train:
+			self.dx=self.dout*self.dp_array
+		else:
+			self.dx=None
+			self.dout=None
+	def update(self,train=True):
 		pass
 # Batch_normalization类
 class batch_normalization(layer):
@@ -166,10 +192,11 @@ class batch_normalization(layer):
 		self.dbeta = None
 		self.dgamma = None
 
-	def forward(self,train):
+	def forward(self,train=True):
 		if train:
-			if not (self.beta and self.gamma):
+			if self.beta is None:
 				self.beta=np.zeros(self.input.shape[1],dtype=np.float)
+			if self.gamma is None:
 				self.gamma = np.ones(self.input.shape[1], dtype=np.float)
 			self.x_mean=np.mean(self.input,axis=0)
 			self.x_std=np.var(self.input,axis=0)
@@ -179,24 +206,30 @@ class batch_normalization(layer):
 		else:
 			self.output=self.input
 
-	def backward(self):
-		x_mu=self.input-self.x_mean
-		std_inv=1./np.sqrt(self.x_std+self.epsilon)
-		self.dx_norm = self.dout*self.gamma
-		self.dx_std = np.sum(self.dx_norm * x_mu, axis=0)
-		self.dx_std *= -.5
-		self.dx_std *= std_inv ** 3
-		self.dx_mean = -np.sum(self.dx_norm *std_inv, axis=0)
-		self.dx_mean += self.dx_std * np.mean(-2. * x_mu, axis=0)
-		self.dx = (self.dx_norm * std_inv)
-		self.dx += (self.dx_std * 2 * x_mu / self.batch_cnt)
-		self.dx += (self.dx_mean / self.batch_cnt)
-		self.dgamma = np.sum(self.dout * self.x_norm, axis=0)
-		self.dbeta = np.sum(self.dout, axis=0)
+	def backward(self,train=True):
+		if train:
+			batch_cnt=self.dout.shape[0]
+			x_mu=self.input-self.x_mean
+			std_inv=1./np.sqrt(self.x_std+self.epsilon)
+			self.dx_norm = self.dout*self.gamma
+			self.dx_std = np.sum(self.dx_norm * x_mu, axis=0)
+			self.dx_std *= -.5
+			self.dx_std *= std_inv ** 3
+			self.dx_mean = -np.sum(self.dx_norm *std_inv, axis=0)
+			self.dx_mean += self.dx_std * np.mean(-2. * x_mu, axis=0)
+			self.dx = (self.dx_norm * std_inv)
+			self.dx += (self.dx_std * 2 * x_mu / batch_cnt)
+			self.dx += (self.dx_mean / batch_cnt)
+			self.dgamma = np.sum(self.dout * self.x_norm, axis=0)
+			self.dbeta = np.sum(self.dout, axis=0)
+		else:
+			self.dx=None
+			self.dout=None
 
-	def update(self):
-		self.beta += self.dbeta
-		self.gamma += self.dgamma
+	def update(self,train=True):
+		if train:
+			self.beta += self.dbeta
+			self.gamma += self.dgamma
 # 全连接类
 class full_connect(layer):
 	para=None
@@ -212,31 +245,37 @@ class full_connect(layer):
 		self.para=None
 		self.opti=None
 		self.dpara=None
-	def forward(self,train):
+	def forward(self,train=True):
 		cnt=self.input.shape[0]
-		if not self.para:
+		if self.para is None:
 			self.para=np.random.uniform(0.01 * 0.01, 0.02 * 0.02,[self.my_cnt,self.parent_cnt+1])
 		self.output=np.empty([cnt,self.my_cnt],dtype=np.float)
 		for i in range(cnt):
-			self.output[i,:]=  np.dot(self.para[:, :self.parent_cnt], input)
+			self.output[i,:]=  np.dot(self.para[:, :self.parent_cnt], self.input[i,:])
 			self.output[i, :] += self.para[:, self.parent_cnt]
-	def backward(self):
-		cnt = self.dout.shape[0]
-		self.dpara=np.empty([cnt,self.my_cnt,self.parent_cnt+1],dtype=np.float)
-		self.dx=np.zeros([cnt,self.parent_cnt],dtype=np.float)
-		# 计算dx
-		for i in range(cnt):
-			for j in range(self.my_cnt):
-				self.dx[i,:] += self.dout[i,j] * self.para[j, :self.parent_cnt]
-		# 通过dx和input来计算dpara
-		self.dpara[:,:,self.parent_cnt]=self.dout
-		for i in range(self.parent_cnt):
-			self.dpara[:,:,i] = self.dpara[:,:,self.parent_cnt] * self.input[:,i]
-	def update(self):
-		self.opti.input = self.para
-		self.opti.dx = np.average(self.dpara,axis=0)
-		self.opti.update()
-		self.para=self.opti.output
+	def backward(self,train=True):
+		if train:
+			cnt = self.dout.shape[0]
+			self.dpara=np.empty([cnt,self.my_cnt,self.parent_cnt+1],dtype=np.float)
+			self.dx=np.zeros([cnt,self.parent_cnt],dtype=np.float)
+			# 计算dx
+			for i in range(cnt):
+				for j in range(self.my_cnt):
+					self.dx[i,:] += self.dout[i,j] * self.para[j, :self.parent_cnt]
+			# 通过dx和input来计算dpara
+			self.dpara[:,:,self.parent_cnt]=self.dout
+			for i in range(self.parent_cnt):
+				for j in range(cnt):
+					self.dpara[j,:,i] = self.dpara[j,:,self.parent_cnt] * self.input[j,i]
+		else:
+			self.dx=None
+			self.dout=None
+	def update(self,train=True):
+		if train:
+			self.opti.input = self.para
+			self.opti.dx = np.average(self.dpara,axis=0)
+			self.opti.update()
+			self.para=self.opti.output.copy()
 
 # 迭代优化的抽象类
 class optimization:
@@ -271,17 +310,17 @@ class adam(optimization):
 		self.mt=None
 		self.vt=None
 	def update(self):
-		if not self.mt:
-			self.mt=np.zeros(self.input.shape,dtype=np.float)
-		if not self.vt:
-			self.vt=np.zeros(self.input.shape,dtype=np.float)
+		if self.mt is None:
+			self.mt=0
+		if self.vt is None:
+			self.vt=0
 		gt = self.input
-		alpha = ratio
-		self.mt = beta1 * self.mt + (1. - beta1) * gt
-		self.vt = beta2 * self.vt + (1. - beta2) * gt ** 2
-		mt_mean = self.mt / (1. - beta1)
-		vt_mean = self.vt / (1. - beta2)
-		self.output=self.input+mt_mean / np.sqrt(vt_mean + epsilon) * alpha
+		alpha = self.ratio
+		self.mt = self.beta1 * self.mt + (1. - self.beta1) * gt
+		self.vt = self.beta2 * self.vt + (1. - self.beta2) * gt ** 2
+		mt_mean = self.mt / (1. - self.beta1)
+		vt_mean = self.vt / (1. - self.beta2)
+		self.output=self.input+mt_mean / np.sqrt(vt_mean + self.epsilon) * alpha
 
 
 
