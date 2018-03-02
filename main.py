1.加入batch normalization在预测时的均值与方差估计，BN层完美实现 2.功能简单测试应该是没有啥大bug了。

main.py
@@ -11,10 +11,10 @@ path_images1 = ".\\source\\t10k-images.idx3-ubyte"
 # 对原始图片进行标准化处理
 def normflat(data):
 	remap=data.flatten()
-	#mean=np.mean(remap)
-	#deviation=np.std(remap)
-	#remap=(remap-mean)/np.sqrt(deviation)
-	remap=remap/255
+	mean=np.mean(remap)
+	deviation=np.std(remap)
+	remap=(remap-mean)/np.sqrt(deviation+1e-8)
+	#remap=remap/255
 	return remap
 
 # initiate a nn
@@ -23,15 +23,15 @@ def creatnn():
 	fc1=mnn.full_connect(28*28,100)
 	fc2=mnn.full_connect(100,100)
 	fc3=mnn.full_connect(100,10)
-	fc1.opti=mnn.adam(0.1)
-	fc2.opti=mnn.adam(0.1)
-	fc3.opti=mnn.adam(0.1)
+	fc1.opti=mnn.adam(0.001)
+	fc2.opti=mnn.adam(0.001)
+	fc3.opti=mnn.adam(0.001)
 	nn.addlayer(fc1)
 	nn.addlayer(mnn.batch_normalization())
-	nn.addlayer(mnn.active_function(1))
+	nn.addlayer(mnn.active_function(2))
 	nn.addlayer(fc2)
 	nn.addlayer(mnn.batch_normalization())
-	nn.addlayer(mnn.active_function(1))
+	nn.addlayer(mnn.active_function(2))
 	nn.addlayer(fc3)
 	nn.addlayer(mnn.active_function(3))
 	return nn
@@ -39,11 +39,12 @@ def creatnn():
 def test_accu(filename, count, datas, results):
 	right = 0
 	wrong = 0
-	nn1=mnn.mnn.load(filename)
+	nn1=mnn.mnn()
+	nn1=nn1.load(filename)
 	for index in range(count):
-		nn1.input=datas[index:index,:]
+		nn1.input=datas[index,:].reshape([1,28*28])
 		nn1.forward(False)
-		if nn1.output[results[index]] > 0.5 :
+		if nn1.output[0,results[index]] > 0.5 :
 			right += 1
 		else:
 			wrong += 1
@@ -82,10 +83,10 @@ if __name__ == "__main__":
 	for i in range(fp1_cnt):
 		img1[i,:]=normflat(fp1_img[i,:,:])
 
-	cat = 100
-	group = 10000
-	nn1 = creatnn()
+	cat = 400   #每次mini-batch随机抽取100个样本
+	group = 1000
 	first = True
+	nn1 = creatnn()
 	error = np.zeros(group, dtype=float)
 	rs = np.zeros(cat, dtype=float)
 	if first:
@@ -95,22 +96,20 @@ if __name__ == "__main__":
 			# nn1.clear()
 			if i == 100 - 1:
 				nn1.save("d:\\paras\\parad100.pkl")
+			if i == 200 - 1:
+				nn1.save("d:\\paras\\parad200.pkl")
+			if i == 500 - 1:
+				nn1.save("d:\\paras\\parad500.pkl")
 			if i == 1000 - 1:
 				nn1.save("d:\\paras\\parad1000.pkl")
-			if i == 2000 - 1:
-				nn1.save("d:\\paras\\parad2000.pkl")
-			if i == 5000 - 1:
-				nn1.save("d:\\paras\\parad5000.pkl")
-			if i == 10000 - 1:
-				nn1.save("d:\\paras\\parad10000.pkl")
 
 			sgd = np.random.randint(0, fl_cnt, size=cat)
 			while res > 1e-5 and cnt < 2:
 				k = 0
 				result = np.zeros([cat,10],dtype=np.float)
-				for j in sgd:
-					result[k,fl_idx[j]]=1
-					k+=1
+				one = np.array([range(cat),fl_idx[sgd]])
+				for j in range(cat):
+					result[one[0],one[1]]=1
 				#开始训练
 				nn1.input=img[sgd,:]
 				nn1.forward(True,result)
@@ -127,10 +126,9 @@ if __name__ == "__main__":
 
 	print("总测试样本的个数是：", fl1_cnt, "。")
 	print("迭代100次后的准确率是：", test_accu("d:\\paras\\parad100.pkl", fl1_cnt, img1, fl1_idx) * 100, "%  ")
+	print("迭代200次后的准确率是：", test_accu("d:\\paras\\parad200.pkl", fl1_cnt, img1, fl1_idx) * 100, "%  ")
+	print("迭代500次后的准确率是：", test_accu("d:\\paras\\parad500.pkl", fl1_cnt, img1, fl1_idx) * 100, "%  ")
 	print("迭代1000次后的准确率是：", test_accu("d:\\paras\\parad1000.pkl", fl1_cnt, img1, fl1_idx) * 100, "%  ")
-	print("迭代2000次后的准确率是：", test_accu("d:\\paras\\parad2000.pkl", fl1_cnt, img1, fl1_idx) * 100, "%  ")
-	print("迭代5000次后的准确率是：", test_accu("d:\\paras\\parad5000.pkl", fl1_cnt, img1, fl1_idx) * 100, "%  ")
-	print("迭代10000次后的准确率是：", test_accu("d:\\paras\\parad10000.pkl", fl1_cnt, img1, fl1_idx) * 100, "%  ")
 	error = np.load("d:\\paras\\error.npy")
 	plt.figure()
 	plt.subplot(211)

venv/mnn.py
@@ -23,13 +23,17 @@ class mnn:
 		object.child=None
 	def forward(self,train=True,result=None):
 		p=self.head
+		k=0
 		while p :
 			if p.parent:
 				p.input=p.parent.output
 			else:
 				p.input = self.input
 			p.forward(train)
+			#if isinstance(p,full_connect):
+			#	print ("第",k,"层全连接：",np.average(np.abs(p.para)),np.average(np.abs(p.output)))
 			p=p.child
+			k+=1
 		self.output=self.tail.output
 		if train:
 			self.diff=result-self.output
@@ -176,6 +180,11 @@ class batch_normalization(layer):
 	dx_mean = None
 	dbeta = None
 	dgamma = None
+	#运行时均值和方差
+	beta1 = 0.9
+	beta2 = 0.99
+	running_mean = 0
+	running_std = 0
 
 	def __del__(self):
 		layer.__del__(self)
@@ -203,8 +212,11 @@ class batch_normalization(layer):
 			self.x_norm=(self.input-self.x_mean)/np.sqrt(self.x_std+self.epsilon)
 			self.output=self.x_norm*self.gamma
 			self.output+=self.beta
+			self.running_mean = self.beta1*self.running_mean+(1-self.beta1)*self.x_mean
+			self.running_std = self.beta2*self.running_std+(1-self.beta2)*self.x_std
 		else:
-			self.output=self.input
+			x_norm=(self.input-self.running_mean)/np.sqrt(self.running_std+self.epsilon)
+			self.output=x_norm*self.gamma+self.beta
 
 	def backward(self,train=True):
 		if train:
@@ -225,6 +237,9 @@ class batch_normalization(layer):
 		else:
 			self.dx=None
 			self.dout=None
+			self.dpara=None
+			self.dbeta=None
+			self.dgamma=None
 
 	def update(self,train=True):
 		if train:
@@ -263,10 +278,10 @@ class full_connect(layer):
 				for j in range(self.my_cnt):
 					self.dx[i,:] += self.dout[i,j] * self.para[j, :self.parent_cnt]
 			# 通过dx和input来计算dpara
-			self.dpara[:,:,self.parent_cnt]=self.dout
+			self.dpara[:,:,self.parent_cnt]=self.dout.copy()
 			for i in range(self.parent_cnt):
 				for j in range(cnt):
-					self.dpara[j,:,i] = self.dpara[j,:,self.parent_cnt] * self.input[j,i]
+					self.dpara[j,:,i] = self.dout[j,:] * self.input[j,i]
 		else:
 			self.dx=None
 			self.dout=None
@@ -311,10 +326,10 @@ class adam(optimization):
 		self.vt=None
 	def update(self):
 		if self.mt is None:
-			self.mt=0
+			self.mt=np.zeros(self.input.shape,dtype=np.float)
 		if self.vt is None:
-			self.vt=0
-		gt = self.input
+			self.vt=np.zeros(self.input.shape,dtype=np.float)
+		gt = self.dx
 		alpha = self.ratio
 		self.mt = self.beta1 * self.mt + (1. - self.beta1) * gt
 		self.vt = self.beta2 * self.vt + (1. - self.beta2) * gt ** 2